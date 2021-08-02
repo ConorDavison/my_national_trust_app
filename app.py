4be91d3eaeaa004e8dec104874b52fbb23015f09
ConorDavison <conorjamesdavison@hotmail.co.uk>
2021-08-02 15:41:22 +0000

    added login functionality to index.html

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def home():
         user = mongo.db.users.find_one(
             {'username': session['user']})
 
-        return render_template('profile.html')
+        return render_template('index.html')
 
     else:
 
@@ -60,6 +60,29 @@ def register():
         session['user'] = request.form.get("username").lower()
         flash("You have successfully registered, welcome..")
     return render_template('register.html')
+
+
+@app.route('/home', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(
+                existing_user['password'], request.form.get('password')):
+                session["user"] = request.form.get("username").lower()
+                flash(
+                    "Welcome {}, hope you're well".format(
+                        request.form.get("username")))
+            else: 
+                flash('Username and/or Password is incorrect')
+                return redirect(url_for('login'))
+
+        else:
+            flash("Username and/or Password is incorrect")
+            return redirect(url_for('login'))
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
