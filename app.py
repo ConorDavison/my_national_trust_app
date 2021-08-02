9e8a044a1c6bae0959c8b8d20bf768e93c12d859
ConorDavison <conorjamesdavison@hotmail.co.uk>
2021-08-02 14:06:58 +0000

    added register template and adjusted color theme

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
      redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -30,12 +31,17 @@ def home():
     if "user" in session:
         user = mongo.db.users.find_one(
             {'username': session['user']})
-        
+
         return render_template('profile.html')
-    
+
     else:
 
         return render_template('index.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 
 if __name__ == "__main__":
